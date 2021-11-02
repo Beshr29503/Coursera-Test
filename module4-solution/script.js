ae6a676b95230cbd56d600abaeacd7b7e9f1ea22
Ibrahim Beshr <himabishr@gmail.com>
2021-11-02 13:07:27 +0200

    I am back!

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -32,16 +32,13 @@ WARNING!!! WARNING!!!
 // STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
-(function () {
-
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+// (Note, Step 2 will be done in the SpeakHello.js file.) //
 
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Loop over the names array and say either 'Hello' or "Good Bye"
-// using either the helloSpeaker's or byeSpeaker's 'speak' method.
-// See Lecture 50, part 1
-for (var i = 0; i < names.length; i++) {
+// using either the helloSpeaker's or byeSpeaker's 'speak' method. 
+// See Lecture 50, part 1 //
+
 
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Retrieve the first letter of the current name in the loop.
@@ -49,19 +46,22 @@ for (var i = 0; i < names.length; i++) {
   // names that start with either upper case or lower case 'J'/'j', call
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  var firstLetter = names[i].charAt(0).toLowerCase();
+  // Look up these methods on Mozilla Developer Network web site if needed. //
+
 
   // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-  if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
-  } else {
-    helloSpeaker.speak(names[i]);
-  }
-}
+  // name in the loop. //
 
-})();
+
+let names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (let i = 0; i < names.length; i++) {
+let curName = names[i];
+  if (curName[0].toLowercase() === 'j') {
+    byeSpeaker.speak(curName);
+  }
+  else {
+    helloSpeaker.speak(curName);
+  }
